ab4be05c4001f3b7be19e14f818b4c86cf6528e0
Luffy529 <youssefnafaa529@gmail.com>
2023-09-15 17:46:50 +0100

    add descruption

--- 0-main.c
+++ 0-main.c
@@ -1,5 +1,10 @@
 #include "main.h"
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char *name = "abakkar";
@@ -9,8 +14,10 @@ int main(void)
 	_printf("Aissam abakka%c", 'r');
 	_putchar('\n');
 	_printf("this is %%");
-	
 	_putchar('\n');
-
+	_printf("%d", 24);/* by youssef */
+	_putchar('\n');
+	_printf("%i", -200);
+	_putchar('\n');
 	return (0);
 }
